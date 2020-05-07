419752a5ad9824fad3111a571ff53e6665ebeba9
j721 <juliadvu@gmail.com>
2020-05-07 12:18:23 -0500

    added changes for heroku deployment

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 // code away!
 const server = require('./server.js');
 
-server.listen(2000, ()=>{
-    console.log('\n* Server Running on http://localhost:2000 *\n')
-})
+const port = process.env.PORT || 2000;
+
+server.listen(port, () => {
+    console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
+  });
+
+
+// server.listen(2000, ()=>{
+//     console.log('\n* Server Running on http://localhost:2000 *\n')
+// })
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
+  res.status(200).json({environment: process.env.NODE_ENV});
 });
 
 //custom middleware
